00bad08c72c63b6d5e87793d148fbbf97c449e91
puthon-esmat <esmatrasouli69@gmail.com>
2023-07-19 19:52:10 +0500

    todays lessons

---
+++ boolean.py
@@ -0,0 +1,49 @@
+a = 320
+b = 232
+
+if b > a:
+    print("b is greather than a")
+else:
+    print("b is not greather than a")
+    
+    
+    
+    print(2 > 1)
+    print(2 < 1)
+    print(2 == 2)
+    
+    
+    print(bool("hey python"))
+    print(bool())
+    print(bool(5))
+    
+    
+    a = 10 
+    b = 20 
+    c = 30 
+    
+    if a < b and b < c:
+        print("your correct!")
+
+
+    if a < b or b > c:
+        print("its right")
+            
+    
+x = ["apple", "banana"]
+print("banana"in x)
+
+    
+x = ["ahmad", "mohammad", "esmat", "rasouli", "farid"]
+print("omid" not in x )
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ pythonLists.py
@@ -0,0 +1,19 @@
+nameslist =["esmat" , "rasouli", "mohammad", "ahmad"]
+print(nameslist)
+print(len(nameslist))
+
+
+print(nameslist[1])
+
+print(nameslist[1:3])
+
+print(nameslist[:3])
+print(nameslist[1:])
+
+
+if "qand agha" in nameslist:
+    print(present)
+else
+print(absent)
+
+    
